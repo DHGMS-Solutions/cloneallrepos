7a1d41a2c9a5bc93e04033b227ed0913326ead81
David Vreony <dpvreony@users.noreply.github.com>
2016-12-27 21:47:50 +0000

    unit tests and nuget updates

--- src/Dhgms.CloneAllRepos.Cmd/Program.cs
+++ src/Dhgms.CloneAllRepos.Cmd/Program.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Dhgms.CloneAllRepos.Cmd
+﻿namespace Dhgms.CloneAllRepos.Cmd
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             // Get settings from command line
             var commandLineArgs = new CommandLineArguments();
---
+++ src/Dhgms.CloneAllRepos.UnitTests/CommandLineArgumentsTest.cs
@@ -0,0 +1,70 @@
+﻿namespace Dhgms.CloneAllRepos.UnitTests
+{
+    using Cmd;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public static class CommandLineArgumentsTest
+    {
+        public sealed class ParseArgumentsMethod
+        {
+            public static string testApiKey = "TESTAPIKEY";
+
+            public static IEnumerable<object[]> ApiKeyTestData => new List<object[]>
+            {
+                new object[]
+                {
+                    "k",
+                    testApiKey
+                },
+
+                new object[]
+                {
+                    "apikey",
+                    testApiKey
+                },
+            };
+
+            public static IEnumerable<object[]> WhatIfTestData => new List<object[]>
+            {
+                new object[]
+                {
+                    "w"
+                },
+
+                new object[]
+                {
+                    "whatif"
+                },
+            };
+
+            [Theory]
+            [MemberData(nameof(ApiKeyTestData))]
+            public void ApiKey(string[] args)
+            {
+                var commandLineArgs = new CommandLineArguments();
+                var success = CommandLine.Parser.Default.ParseArguments(args, commandLineArgs);
+
+                Assert.True(success);
+                Assert.Equal(testApiKey, commandLineArgs.ApiKey);
+                Assert.False(commandLineArgs.WhatIf);
+            }
+
+            [Theory]
+            [MemberData(nameof(WhatIfTestData))]
+            public void WhatIfSpecified(string[] args)
+            {
+                var commandLineArgs = new CommandLineArguments();
+                var success = CommandLine.Parser.Default.ParseArguments(args, commandLineArgs);
+
+                Assert.True(success);
+                Assert.Null(commandLineArgs.ApiKey);
+                Assert.False(commandLineArgs.WhatIf);
+            }
+        }
+    }
+}
