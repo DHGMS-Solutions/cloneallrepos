0cc01abbfdfabbabc211ac898986a01ba0c48880
David Vreony <dpvreony@users.noreply.github.com>
2018-01-01 14:56:50 +0000

    verb jobs wrapped

--- src/Dhgms.CloneAllRepos.Cmd/BaseVerbBasedConsoleAppJob.cs
+++ src/Dhgms.CloneAllRepos.Cmd/BaseVerbBasedConsoleAppJob.cs
@@ -1,4 +1,6 @@
-﻿namespace Dhgms.CloneAllRepos.Cmd
+﻿using Foundatio.AsyncEx.Synchronous;
+
+namespace Dhgms.CloneAllRepos.Cmd
 {
     using System;
     using System.Collections.Generic;
@@ -12,20 +14,20 @@
     using MediatR;
 
     public abstract class BaseVerbBasedConsoleAppJob<T1Verb, T1Job, T2Verb, T2Job, T3Verb, T3Job> : IRequestHandler<StringArrayRequest<int>, int>
-        where T1Job : IRequestHandler<T1Verb, int>
-        where T1Verb : IRequest<int>
-        where T2Job : IRequestHandler<T2Verb, int>
-        where T2Verb : IRequest<int>
-        where T3Job : IRequestHandler<T3Verb, int>
-        where T3Verb : IRequest<int>
+        where T1Job : IRequestHandler<T1Verb>
+        where T1Verb : IRequest
+        where T2Job : IRequestHandler<T2Verb>
+        where T2Verb : IRequest
+        where T3Job : IRequestHandler<T3Verb>
+        where T3Verb : IRequest
     {
         public async Task<int> Handle(StringArrayRequest<int> request, CancellationToken cancellationToken)
         {
             return await Task.FromResult(Parser.Default.ParseArguments<T1Verb, T2Verb, T3Verb>(request.Data)
                 .MapResult(
-                    (T1Verb opts) => GetT1Job().Handle(opts, cancellationToken).Result,
-                    (T2Verb opts) => GetT2Job().Handle(opts, cancellationToken).Result,
-                    (T3Verb opts) => GetT3Job().Handle(opts, cancellationToken).Result,
+                    (T1Verb opts) => RunJob(GetT1Job, opts, 2, cancellationToken),
+                    (T2Verb opts) => RunJob(GetT2Job, opts, 3, cancellationToken),
+                    (T3Verb opts) => RunJob(GetT3Job, opts, 4, cancellationToken),
                     _ => 1));
         }
 
@@ -34,5 +36,26 @@
         protected abstract T2Job GetT2Job();
 
         protected abstract T3Job GetT3Job();
+
+        private int RunJob<TJob, TVerb>(
+            Func<TJob> getJob,
+            TVerb opts,
+            int failureCode,
+            CancellationToken cancellationToken)
+            where TJob : IRequestHandler<TVerb>
+            where TVerb : IRequest
+        {
+            try
+            {
+                var job = getJob();
+                job.Handle(opts, cancellationToken).WaitAndUnwrapException(cancellationToken);
+            }
+            catch (Exception e)
+            {
+                return failureCode;
+            }
+
+            return 0;
+        }
     }
 }
