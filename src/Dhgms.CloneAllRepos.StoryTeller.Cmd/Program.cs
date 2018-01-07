19fce0c338250d8a18cf909d0039615ecd210f34
David Vreony <dpvreony@users.noreply.github.com>
2018-01-07 21:10:57 +0000

    added storyteller

---
+++ src/Dhgms.CloneAllRepos.StoryTeller.Cmd/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using StoryTeller;
+
+namespace Dhgms.CloneAllRepos.StoryTeller.Cmd
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            // If you do not need a custom ISystem
+            StorytellerAgent.Run(args);
+        }
+    }
+}
