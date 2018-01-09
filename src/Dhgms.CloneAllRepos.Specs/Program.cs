972ce0eddd138f733be319353f4267a120532094
David Vreony <dpvreony@users.noreply.github.com>
2018-01-09 21:42:29 +0000

    placeholder for specflow

---
+++ src/Dhgms.CloneAllRepos.Specs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dhgms.CloneAllRepos.Specs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
