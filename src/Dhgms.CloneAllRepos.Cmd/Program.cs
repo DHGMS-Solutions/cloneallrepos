60e5648dbb6a79f9d7f3de3f395301278a53ff69
David Vreony <dpvreony@users.noreply.github.com>
2018-01-23 21:19:06 +0000

    nlog config

--- src/Dhgms.CloneAllRepos.Cmd/Program.cs
+++ src/Dhgms.CloneAllRepos.Cmd/Program.cs
@@ -1,9 +1,13 @@
-﻿using System.Linq;
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading;
 using SystemWrapper.IO;
 using Microsoft.Extensions.Logging;
 using Nito.AsyncEx.Synchronous;
+using NLog.Config;
 using NLog.Extensions.Logging;
+using NLog.Targets;
 
 namespace Dhgms.CloneAllRepos.Cmd
 {
@@ -13,13 +17,37 @@ namespace Dhgms.CloneAllRepos.Cmd
         {
             using (var loggerFactory = new LoggerFactory())
             {
-                loggerFactory.AddNLog(new NLogProviderOptions { CaptureMessageTemplates = true, CaptureMessageProperties =true });
-                loggerFactory.ConfigureNLog("nlog.config");
+                ConfigureLogging(loggerFactory);
 
                 // todo : update to dotnetcore and use an async main
                 var consoleAppJob = new ConsoleAppJob(loggerFactory);
                 return consoleAppJob.Handle(args).Result;
             }
+        }
+
+        private static void ConfigureLogging(ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddNLog(new NLogProviderOptions { CaptureMessageTemplates = true, CaptureMessageProperties =true });
+            var config = new LoggingConfiguration();
+            var consoleTarget = new ColoredConsoleTarget();
+            config.AddTarget("console", consoleTarget);
+            var fileTarget = new FileTarget();
+            config.AddTarget("file", fileTarget);
+
+            consoleTarget.Layout = @"${date:format=HH\:mm\:ss} ${logger} ${message}";
+            var localAppDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+            fileTarget.FileName = $"{localAppDataPath}/dhgms solutions/cloneallrepos.cmd/nlogfile.txt";
+            fileTarget.Layout = "${message}";
+
+            var consoleLogLevel = Debugger.IsAttached ? NLog.LogLevel.Debug : NLog.LogLevel.Info;
+            var rule1 = new LoggingRule("*", consoleLogLevel, consoleTarget);
+            config.LoggingRules.Add(rule1);
+
+            var rule2 = new LoggingRule("*", NLog.LogLevel.Debug, fileTarget);
+            config.LoggingRules.Add(rule2);
+
+            loggerFactory.ConfigureNLog(config);
         }
     }
 }
